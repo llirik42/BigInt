25bd477186cdff44e654367d1a2c30ec72367bbc
Kirill Kondrenko <63352302+llirik42@users.noreply.github.com>
2022-09-11 12:02:30 +0700

    Added empty constructor and attempt of std::cout for BigInt

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 #include <cmath>
 
-#define SIZE_OF_BYTE 8
-#define POW_2_SIZE_OF_BYTE 256
-#define GET_BIT_8(N, I) ((1 << (SIZE_OF_BYTE - (I) - 1) & (N)) >> (SIZE_OF_BYTE - (I) - 1))
+#define BASE 256
+#define GET_BIT_8(N, I) ((1 << (I) & (N)) >> (I))
 
-unsigned int ceil_log8(unsigned int number){
+unsigned int ceil_log(unsigned int base, unsigned int number){
     if (!number){
         return 1;
     }
@@ -14,7 +13,7 @@ unsigned int ceil_log8(unsigned int number){
 
     while (number){
         result++;
-        number /= POW_2_SIZE_OF_BYTE;
+        number /= base;
     }
 
     return result;
@@ -28,26 +27,35 @@ private:
     unsigned long long data_length;
     bool is_positive;
 public:
-    BigInt();
+    BigInt(){
+        data_length = 1;
+
+        data = new unsigned char[1];
+        data[0] = 0;
+
+        is_positive = true;
+
+        //std::cout << "Positive=" << is_positive << " data_length=" << data_length << "\n";
+
+        //for (unsigned int i = 0; i < data_length; i++){
+          //  std::cout << int(data[i]) << " ";
+        //}
+    }
     explicit BigInt(int number){
         is_positive = (number >= 0);
 
         unsigned int abs_number = abs(number);
 
-        data_length = ceil_log8(abs_number);
+        data_length = ceil_log(BASE, abs_number);
 
         data = new unsigned char[data_length];
 
         for (unsigned int i = data_length; i > 0; i--){
-            data[i - 1] = abs_number % POW_2_SIZE_OF_BYTE;
-            abs_number /= POW_2_SIZE_OF_BYTE;
+            data[i - 1] = abs_number % BASE;
+            abs_number /= BASE;
         }
 
-        std::cout << "Positive=" << is_positive << " data_length=" << data_length << "\n";
 
-        for (unsigned int i = 0; i < data_length; i++){
-            std::cout << int(data[i]) << " ";
-        }
     }
     BigInt(std::string){
 
@@ -92,8 +100,42 @@ public:
     operator int() const;
     operator std::string() const;
 
-    size_t size() const;  // size in bytes
+    [[nodiscard]] size_t size() const{
+        return sizeof(data_length) + sizeof(is_positive) + data_length;
+    }
 
+    [[nodiscard]] std::string representation() const{
+        std::string binary_representation;
+
+        for (unsigned long long i = data_length; i > 0; i--){
+            const unsigned char current_block = data[i - 1];
+
+            for (unsigned char j = 0; j < 8; j++){
+                binary_representation.insert(0, 1, GET_BIT_8(current_block, j) + '0');
+            }
+        }
+
+        std::string representation;
+        const unsigned long long binary_representation_length = binary_representation.length();
+
+        unsigned char carry = 0;
+        for (unsigned long long i = binary_representation_length; i > 0; i--){
+            const unsigned char current_binary_digit = binary_representation[i - 1];
+            const unsigned long long
+
+
+
+            carry = binary_representation[i - 1] / 10;
+            representation.insert(0, 1, binary_representation[i - 1] % 10);
+        }
+
+
+
+
+
+
+        return representation;
+    }
 
 };
 
@@ -108,8 +150,7 @@ BigInt operator|(const BigInt&, const BigInt&);
 
 
 std::ostream& operator<<(std::ostream& out, const BigInt& i){
-
-
+    out << i.representation();
     return out;
 }
 std::istream& operator>>(std::istream& o, BigInt& i);
@@ -117,7 +158,10 @@ std::istream& operator>>(std::istream& o, BigInt& i);
 
 
 int main() {
-    BigInt b = BigInt(3123213);
+    //BigInt a = BigInt(1234567890);
+    BigInt b = BigInt(2);
+
+    std::cout << b;
 
     return 0;
 }
