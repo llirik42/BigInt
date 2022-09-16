337a16c76d9041c8e735ff70834dc38b5f5cb9d5
Kirill Kondrenko <63352302+llirik42@users.noreply.github.com>
2022-09-17 00:54:58 +0700

    Moved some utils to utils.h

--- src/utils.h
+++ src/utils.h
@@ -1,9 +1,27 @@
 inline unsigned int get_max(unsigned int a, unsigned int b){
     return a > b ? a : b;
 }
+inline unsigned int get_min(unsigned int a, unsigned int b){
+    return a > b ? b : a;
+}
 inline unsigned int char_to_digit(unsigned char c){
     return c - '0';
 }
 inline char digit_to_char(unsigned int d){
     return char(d + '0');
+}
+
+static unsigned int ceil_log(unsigned long long base, unsigned long long number){
+    if (!number){
+        return 1;
+    }
+
+    unsigned int result = 0;
+
+    while (number){
+        result++;
+        number /= base;
+    }
+
+    return result;
 }
