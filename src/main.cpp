02d74967bde5c2aea4276e12755f626275fdbe26
Kirill Kondrenko <63352302+llirik42@users.noreply.github.com>
2022-09-17 00:54:14 +0700

    added more tests for NumericString

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,7 @@ void test_numeric_string(){
     assert(NumericString(42) == NumericString("42"));
     assert(NumericString(0) + NumericString(111) == NumericString(111));
     assert(NumericString(999) + NumericString(1) == NumericString(1000));
+    assert(NumericString(42) * NumericString(1) == NumericString(42));
     assert(NumericString(1) * NumericString(42) == NumericString(42));
     assert(NumericString(0) * NumericString(1) == NumericString(0));
     assert(NumericString(0) * NumericString(0) == NumericString(0));
@@ -21,7 +22,7 @@ void test_numeric_string(){
     assert(NumericString(42) / 5 == "8");
     assert(NumericString(0) % 5 == 0);
     assert(NumericString(0) % 42 == 0);
-    assert(NumericString(1234567890).length() == 10);
+    assert(NumericString(1234567890).get_length() == 10);
     assert(NumericString(123456798) > NumericString(123456789));
     assert((unsigned long long)NumericString(123456789) == 123456789);
     assert(std::string(NumericString(123456789)) == "123456789");
