a1291de33f7e23ac3fd021499962ee2ab5c40166
Kirill Kondrenko <63352302+llirik42@users.noreply.github.com>
2022-09-24 22:20:15 +0700

    Minor fixes

--- src/tests/comparison_test.cpp
+++ src/tests/comparison_test.cpp
@@ -1,98 +1,98 @@
 #include "gtest/gtest.h"
 #include "../big_int.h"
 
-TEST(eq_test, eq_test1){
+TEST(comparison_test, eq_test1){
     BigInt a = BigInt("0");
     BigInt b = BigInt(0);
 
     EXPECT_TRUE(a == b);
 }
-TEST(eq_test, eq_test2){
+TEST(comparison_test, eq_test2){
     BigInt a = BigInt("123");
     BigInt b = BigInt(123);
 
     EXPECT_TRUE(a == b);
 }
-TEST(eq_test, eq_test3){
+TEST(comparison_test, eq_test3){
     BigInt a = BigInt("-987");
     BigInt b = BigInt(-987);
 
     EXPECT_TRUE(a == b);
 }
 
-TEST(neq_test, neq_test1){
+TEST(comparison_test, neq_test1){
     BigInt a = BigInt("0");
     BigInt b = BigInt(0);
 
     EXPECT_FALSE(a != b);
 }
-TEST(neq_test, neq_test2){
+TEST(comparison_test, neq_test2){
     BigInt a = BigInt("123");
     BigInt b = BigInt(123);
 
     EXPECT_FALSE(a != b);
 }
-TEST(neq_test, neq_test3){
+TEST(comparison_test, neq_test3){
     BigInt a = BigInt("-987");
     BigInt b = BigInt(-987);
 
     EXPECT_FALSE(a != b);
 }
 
-TEST(greater_test, greater_test1){
+TEST(comparison_test, greater_test1){
     BigInt a = BigInt(1);
     BigInt b = BigInt(0);
     EXPECT_TRUE(a > b);
 }
-TEST(greater_test, greater_test2){
+TEST(comparison_test, greater_test2){
     BigInt a = BigInt(0);
     BigInt b = BigInt(-1);
 
     EXPECT_TRUE(a > b);
 }
-TEST(greater_test, greater_test3){
+TEST(comparison_test, greater_test3){
     BigInt a = BigInt(-123);
     BigInt b = BigInt(-124);
 
     EXPECT_TRUE(a > b);
 }
-TEST(greater_test, greater_test4){
+TEST(comparison_test, greater_test4){
     BigInt a = BigInt(-1);
     BigInt b = BigInt(1);
 
     EXPECT_FALSE(a > b);
 }
 
-TEST(greater_or_equal_test, greater_or_equal_test1){
+TEST(comparison_test, greater_or_equal_test1){
     BigInt a = BigInt(1);
     BigInt b = BigInt(0);
     EXPECT_TRUE(a >= b);
 }
-TEST(greater_or_equal_test, greater_or_equal_test2){
+TEST(comparison_test, greater_or_equal_test2){
     BigInt a = BigInt(0);
     BigInt b = BigInt(-1);
 
     EXPECT_TRUE(a >= b);
 }
-TEST(greater_or_equal_test, greater_or_equal_test3){
+TEST(comparison_test, greater_or_equal_test3){
     BigInt a = BigInt(-123);
     BigInt b = BigInt(-124);
 
     EXPECT_TRUE(a >= b);
 }
-TEST(greater_or_equal_test, greater_or_equal_test4){
+TEST(comparison_test, greater_or_equal_test4){
     BigInt a = BigInt(-1);
     BigInt b = BigInt(1);
 
     EXPECT_FALSE(a >= b);
 }
-TEST(greater_or_equal_test, greater_or_equal_test5){
+TEST(comparison_test, greater_or_equal_test5){
     BigInt a = BigInt(1);
     BigInt b = BigInt(1);
 
     EXPECT_TRUE(a >= b);
 }
-TEST(greater_or_equal_test, greater_or_equal_test6){
+TEST(comparison_test, greater_or_equal_test6){
     BigInt a = BigInt(-1);
     BigInt b = BigInt(-1);
 
--- src/tests/logic_operations_test.cpp
+++ src/tests/logic_operations_test.cpp
@@ -1,122 +1,122 @@
 #include "gtest/gtest.h"
 #include "../big_int.h"
 
-TEST(conjuction_test, conjunction1){
+TEST(logic_operations_test, conjunction1){
     BigInt a = BigInt(0);
     BigInt b = BigInt(0);
     EXPECT_EQ(a & b, BigInt(0));
 }
-TEST(conjuction_test, conjunction2){
+TEST(logic_operations_test, conjunction2){
     BigInt a = BigInt(0);
     BigInt b = BigInt(15);
     EXPECT_EQ(a & b, BigInt(0));
 }
-TEST(conjuction_test, conjunction3){
+TEST(logic_operations_test, conjunction3){
     BigInt a = BigInt(0);
     BigInt b = BigInt("11111111111111111111111");
     EXPECT_EQ(a & b, BigInt(0));
 }
-TEST(conjuction_test, conjunction4){
+TEST(logic_operations_test, conjunction4){
     BigInt a = BigInt(1);
     BigInt b = BigInt(1);
     EXPECT_EQ(a & b, BigInt(1));
 }
-TEST(conjuction_test, conjunction5){
+TEST(logic_operations_test, conjunction5){
     BigInt a = BigInt(1);
     BigInt b = BigInt(-1);
     EXPECT_EQ(a & b, BigInt(-1));
 }
-TEST(conjuction_test, conjunction6){
+TEST(logic_operations_test, conjunction6){
     BigInt a = BigInt(-1);
     BigInt b = BigInt(-1);
     EXPECT_EQ(a & b, BigInt(1));
 }
-TEST(conjuction_test, conjunction7){
+TEST(logic_operations_test, conjunction7){
     BigInt a = BigInt("11111111111");
     BigInt b = BigInt("22222222222");
     EXPECT_EQ(a & b, BigInt("67375494"));
 }
 
-TEST(disjunction_test, disjunction1){
+TEST(logic_operations_test, disjunction1){
     BigInt a = BigInt(0);
     BigInt b = BigInt(0);
     EXPECT_EQ(a | b, BigInt(0));
 }
-TEST(disjunction_test, disjunction2){
+TEST(logic_operations_test, disjunction2){
     BigInt a = BigInt(0);
     BigInt b = BigInt(1);
     EXPECT_EQ(a | b, BigInt(1));
 }
-TEST(disjunction_test, disjunction3){
+TEST(logic_operations_test, disjunction3){
     BigInt a = BigInt(1);
     BigInt b = BigInt(1);
     EXPECT_EQ(a | b, BigInt(1));
 }
-TEST(disjunction_test, disjunction4){
+TEST(logic_operations_test, disjunction4){
     BigInt a = BigInt(1);
     BigInt b = BigInt(-1);
     EXPECT_EQ(a | b, BigInt(-1));
 }
-TEST(disjunction_test, disjunction5){
+TEST(logic_operations_test, disjunction5){
     BigInt a = BigInt(-1);
     BigInt b = BigInt(-1);
     EXPECT_EQ(a | b, BigInt(1));
 }
-TEST(disjunction_test, disjunction6){
+TEST(logic_operations_test, disjunction6){
     BigInt a = BigInt("11111111111");
     BigInt b = BigInt("22222222222");
     EXPECT_EQ(a | b, BigInt("33265957839"));
 }
 
-TEST(xor_test, xor1){
+TEST(logic_operations_test, xor1){
     BigInt a = BigInt(0);
     BigInt b = BigInt(0);
     EXPECT_EQ(a ^ b, BigInt(0));
 }
-TEST(xor_test, xor2){
+TEST(logic_operations_test, xor2){
     BigInt a = BigInt(1);
     BigInt b = BigInt(0);
     EXPECT_EQ(a ^ b, BigInt(1));
 }
-TEST(xor_test, xor3){
+TEST(logic_operations_test, xor3){
     BigInt a = BigInt(1);
     BigInt b = BigInt(1);
     EXPECT_EQ(a ^ b, BigInt(0));
 }
-TEST(xor_test, xor4){
+TEST(logic_operations_test, xor4){
     BigInt a = BigInt(1);
     BigInt b = BigInt(2);
     EXPECT_EQ(a ^ b, BigInt(3));
 }
-TEST(xor_test, xor5){
+TEST(logic_operations_test, xor5){
     BigInt a = BigInt(-1);
     BigInt b = BigInt(-2);
     EXPECT_EQ(a ^ b, BigInt(3));
 }
-TEST(xor_test, xor6){
+TEST(logic_operations_test, xor6){
     BigInt a = BigInt(1);
     BigInt b = BigInt(-2);
     EXPECT_EQ(a ^ b, BigInt(-3));
 }
-TEST(xor_test, xor7){
+TEST(logic_operations_test, xor7){
     BigInt a = BigInt("11111111111");
     BigInt b = BigInt("22222222222");
     EXPECT_EQ(a ^ b, BigInt("33198582345"));
 }
 
-TEST(inversion_test, inversion1){
+TEST(logic_operations_test, inversion1){
     BigInt a = BigInt(0);
     EXPECT_EQ(~a, BigInt(std::to_string(BASE - 1)));
 }
-TEST(inversion_test, inversion2){
+TEST(logic_operations_test, inversion2){
     BigInt a = BigInt(1);
     EXPECT_EQ(~a, BigInt(std::to_string(BASE - 2)));
 }
-TEST(inversion_test, inversion3){
+TEST(logic_operations_test, inversion3){
     BigInt a = BigInt(-1);
     EXPECT_EQ(-(~a) , BigInt(std::to_string(BASE - 2)));
 }
-TEST(inversion_test, inversion4){
+TEST(logic_operations_test, inversion4){
     BigInt a = BigInt(std::to_string(BASE - 1));
     EXPECT_EQ(~a , BigInt(0));
 }
