0d9acec6a1c0874a3cf16885fe9bc7507d022a85
Kirill Kondrenko <63352302+llirik42@users.noreply.github.com>
2022-09-24 22:06:15 +0700

    Changed some titles

--- src/tests/comparison_test.cpp
+++ src/tests/comparison_test.cpp
@@ -1,98 +1,98 @@
 #include "gtest/gtest.h"
 #include "../big_int.h"
 
-TEST(comparison_test, eq_test1){
+TEST(eq_test, eq_test1){
     BigInt a = BigInt("0");
     BigInt b = BigInt(0);
 
     EXPECT_TRUE(a == b);
 }
-TEST(comparison_test, eq_test2){
+TEST(eq_test, eq_test2){
     BigInt a = BigInt("123");
     BigInt b = BigInt(123);
 
     EXPECT_TRUE(a == b);
 }
-TEST(comparison_test, eq_test3){
+TEST(eq_test, eq_test3){
     BigInt a = BigInt("-987");
     BigInt b = BigInt(-987);
 
     EXPECT_TRUE(a == b);
 }
 
-TEST(comparison_test, neq_test1){
+TEST(neq_test, neq_test1){
     BigInt a = BigInt("0");
     BigInt b = BigInt(0);
 
     EXPECT_FALSE(a != b);
 }
-TEST(comparison_test, neq_test2){
+TEST(neq_test, neq_test2){
     BigInt a = BigInt("123");
     BigInt b = BigInt(123);
 
     EXPECT_FALSE(a != b);
 }
-TEST(comparison_test, neq_test3){
+TEST(neq_test, neq_test3){
     BigInt a = BigInt("-987");
     BigInt b = BigInt(-987);
 
     EXPECT_FALSE(a != b);
 }
 
-TEST(comparison_test, more_test1){
+TEST(greater_test, greater_test1){
     BigInt a = BigInt(1);
     BigInt b = BigInt(0);
     EXPECT_TRUE(a > b);
 }
-TEST(comparison_test, more_test2){
+TEST(greater_test, greater_test2){
     BigInt a = BigInt(0);
     BigInt b = BigInt(-1);
 
     EXPECT_TRUE(a > b);
 }
-TEST(comparison_test, more_test3){
+TEST(greater_test, greater_test3){
     BigInt a = BigInt(-123);
     BigInt b = BigInt(-124);
 
     EXPECT_TRUE(a > b);
 }
-TEST(comparison_test, more_test4){
+TEST(greater_test, greater_test4){
     BigInt a = BigInt(-1);
     BigInt b = BigInt(1);
 
     EXPECT_FALSE(a > b);
 }
 
-TEST(comparison_test, more_or_eqal_test1){
+TEST(greater_or_equal_test, greater_or_equal_test1){
     BigInt a = BigInt(1);
     BigInt b = BigInt(0);
     EXPECT_TRUE(a >= b);
 }
-TEST(comparison_test, more_or_eqal_test2){
+TEST(greater_or_equal_test, greater_or_equal_test2){
     BigInt a = BigInt(0);
     BigInt b = BigInt(-1);
 
     EXPECT_TRUE(a >= b);
 }
-TEST(comparison_test, more_or_eqal_test3){
+TEST(greater_or_equal_test, greater_or_equal_test3){
     BigInt a = BigInt(-123);
     BigInt b = BigInt(-124);
 
     EXPECT_TRUE(a >= b);
 }
-TEST(comparison_test, more_or_eqal_test4){
+TEST(greater_or_equal_test, greater_or_equal_test4){
     BigInt a = BigInt(-1);
     BigInt b = BigInt(1);
 
     EXPECT_FALSE(a >= b);
 }
-TEST(comparison_test, more_or_eqal_test5){
+TEST(greater_or_equal_test, greater_or_equal_test5){
     BigInt a = BigInt(1);
     BigInt b = BigInt(1);
 
     EXPECT_TRUE(a >= b);
 }
-TEST(comparison_test, more_or_eqal_test6){
+TEST(greater_or_equal_test, greater_or_equal_test6){
     BigInt a = BigInt(-1);
     BigInt b = BigInt(-1);
 
