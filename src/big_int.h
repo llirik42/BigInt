3adf42ba2945380a0d4845cf466d0d155c655db7
Kirill Kondrenko <63352302+llirik42@users.noreply.github.com>
2022-09-25 19:16:31 +0700

    Added some commentaries

--- src/big_int.h
+++ src/big_int.h
@@ -3,52 +3,57 @@
 
 class BigInt{
 private:
-    bool is_positive;
-    SmartVector vector;
+    bool _is_positive;
+    SmartVector _vector;
 
-    int compare(const BigInt&) const;
+    void handle_minus_zero(); // If _is_positive = false, but object = 0, sets _is_positive = true
 public:
     BigInt();
-    explicit BigInt(int);
-    explicit BigInt(const std::string&);
-    BigInt(const BigInt&);
-    BigInt(BigInt&&) noexcept;
+    explicit BigInt(const int& number);
+    explicit BigInt(const std::string& str);
+    BigInt(const BigInt& b);
+    BigInt(BigInt&& b) noexcept;
     ~BigInt();
 
-    BigInt& operator=(const BigInt&);
-    BigInt& operator=(BigInt&&) noexcept;
+    BigInt& operator=(const BigInt& b);
+    BigInt& operator=(BigInt&& b) noexcept;
 
-    BigInt& operator+=(const BigInt&);
-    BigInt& operator*=(const BigInt&);
-    BigInt& operator-=(const BigInt&);
-    BigInt& operator/=(const BigInt&);
-    BigInt& operator%=(const BigInt&);
+    BigInt& operator+=(const BigInt& b);
+    BigInt& operator*=(const BigInt& b);
+    BigInt& operator-=(const BigInt& b);
+    BigInt& operator/=(const BigInt& b); // Throws std::invalid_argument in case of division by 0
+
+    // Throws std::invalid_argument in case of division by 0, of by negative number
+    // Returns mathematically correct results if dividend < 0. For example, if x = -2, after x % 3, "x" is going to be 1
+    BigInt& operator%=(const BigInt& b);
 
     BigInt operator+() const;
     BigInt operator-();
-    BigInt& operator~();
+    BigInt& operator~(); // Saves the sign, but inverts absolute value on the base of 256
 
     BigInt& operator++();
     const BigInt operator++(int);
     BigInt& operator--();
     const BigInt operator--(int);
 
-    bool operator==(const BigInt&) const;
-    bool operator!=(const BigInt&) const;
-    bool operator<(const BigInt&) const;
-    bool operator>(const BigInt&) const;
-    bool operator<=(const BigInt&) const;
-    bool operator>=(const BigInt&) const;
+    bool operator==(const BigInt& b) const;
+    bool operator!=(const BigInt& b) const;
+    bool operator<(const BigInt& b) const;
+    bool operator>(const BigInt& b) const;
+    bool operator<=(const BigInt& b) const;
+    bool operator>=(const BigInt& b) const;
 
-    BigInt& operator^=(const BigInt&);
-    BigInt& operator&=(const BigInt&);
-    BigInt& operator|=(const BigInt&);
+    // These operations perform operations only with absolute values, sign of result will be like after multiplication
+    BigInt& operator^=(const BigInt& b);
+    BigInt& operator&=(const BigInt& b);
+    BigInt& operator|=(const BigInt& b);
 
-    explicit operator int() const;
+    explicit operator int() const; // If object > INT_MAX returns INT_MAX, if object < INT_MIN, returns INT_MIN
     explicit operator std::string() const;
 
-    [[nodiscard]] size_t size() const;
+    [[nodiscard]] size_t size() const; // Size in bytes for object
     [[nodiscard]] bool is_zero() const;
+    [[nodiscard]] int compare(const BigInt& b) const; // Returns -1, 0, 1 if object <, == or > "b"
 };
 
 BigInt operator+(const BigInt&, const BigInt&);
@@ -61,4 +66,4 @@ BigInt operator&(const BigInt&, const BigInt&);
 BigInt operator|(const BigInt&, const BigInt&);
 
 std::ostream& operator<<(std::ostream&, const BigInt&);
-std::istream& operator>>(std::istream&, BigInt&);
+std::istream& operator>>(std::istream&, BigInt&); // Throws std::invalid_argument if input is incorrect
